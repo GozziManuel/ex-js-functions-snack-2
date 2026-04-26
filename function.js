a50657d9afbd80cfa06d647cdf38ae0d6a7b41eb
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-04-26 17:24:43 +0200

    last bonus

--- function.js
+++ function.js
@@ -1,1 +1,18 @@
+function creaThrottler(func, time) {
+  let Ultima = 0;
+  return function (...args) {
+    const ora = Date.now();
+    if (ora - Ultima >= time) {
+      Ultima = ora;
+      func(...args);
+    } else {
+      console.log("No!");
+    }
+  };
+}
 
+const throttledLog = creaThrottler(() => console.log("Eseguito!"), 2000);
+
+throttledLog(); // ✅ "Eseguito!"
+throttledLog(); // ❌ Ignorato (chiamato troppo presto)
+setTimeout(throttledLog, 2500); // ✅ "Eseguito!" (dopo 2.5 secondi)
